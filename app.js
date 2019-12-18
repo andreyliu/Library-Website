b653684fae4b4714dbec93c99717abc331f18ec9
andreyliu <yuqiliu@andrew.cmu.edu>
2019-12-18 01:14:44 -0500

    deploy:changes for deployment

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(helmet());
 
 const mongoose = require('mongoose');
-const mongoDB = process.env.DB_CONN_STR || '';
+const mongoDB = process.env.MONGODB_URI || '';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
